d20fc0c0f51cda0b93db893960a200371c2bb8f1
Samuel Muli <samuelmuli95@gmail.com>
2024-05-22 14:31:55 +0300

    Add modal

--- src/components/Consulted.vue
+++ src/components/Consulted.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { computed, onMounted, ref } from 'vue';
 import { useStore } from 'vuex';
+import DiaryModal from './DiaryModal.vue';
 
 const store = useStore();
 const user = computed(() => store.getters.getUser)
 const userProfile = computed(() => store.getters.getUserProfile)
 const diaries = computed(() => store.getters.getDiaries)
+const isModalVisible = ref(false)
 
 const calculateAge = (dob) => {
 	const today = new Date()
@@ -22,7 +24,14 @@ const dob = computed(() => new Date(userProfile.value.dateOfBirth.year, userProf
 const age = computed(() => calculateAge(dob.value))
 
 
-// console.log(user.value.loggedInUser);
+const openModal = () => {
+	isModalVisible.value = true
+}
+
+const closeModal = () => {
+	isModalVisible.value = false
+}
+
 onMounted(() => {
 	
 	store.dispatch('setUser')
@@ -35,12 +44,17 @@ onMounted(() => {
     <div>
 			<h1 class="font-semibold mb-10">Consulted</h1>
 			<div class="border-2 border-gray-100 p-5 rounded-2xl">
-				<div class="flex items-center gap-2 mb-5">
-					<img src="../assets/images/smmuli.jpeg" alt="" class="rounded-full h-14">
-					<div>
-						<p class="font-semibold">{{ user ? user.loggedInUser.firstName : '' }} {{ user ? user.loggedInUser.lastName : '' }}</p>
-						<p class="text-xs xl:text-sm text-gray-300">{{ userProfile ? userProfile.gender.value : '' }} - {{ userProfile ? `${age.years} Years` : '' }} {{ userProfile ? `${age.months} Months` : '' }}</p>
+				<div class="flex justify-between mb-5">
+					<div class="flex items-center gap-2">
+						<img src="../assets/images/smmuli.jpeg" alt="" class="rounded-full h-14">
+						<div>
+							<p class="font-semibold">{{ user ? user.loggedInUser.firstName : '' }} {{ user ? user.loggedInUser.lastName : '' }}</p>
+							<p class="text-xs xl:text-sm text-gray-300">{{ userProfile ? userProfile.gender.value : '' }} - {{ userProfile ? `${age.years} Years` : '' }} {{ userProfile ? `${age.months} Months` : '' }}</p>
+						</div>
 					</div>
+					<span class="material-symbols-outlined cursor-pointer" @click="openModal">
+						add
+					</span>
 				</div>
 				<div v-if="diaries.length > 0" class="flex justify-between mb-3">
 					<div v-for="diary in diaries">
@@ -77,7 +91,7 @@ onMounted(() => {
 					<p class="font-normal text-xs text-gray-300 w-8/12">{{ diary.description }}</p>
 				</div>
 				<div v-else class="flex justify-center items-center">
-					<button class="bg-violet-600 rounded-xl p-2 text-white text-xs xl:text-sm">Update Diary</button>
+					<button class="bg-violet-600 rounded-xl p-2 text-white text-xs xl:text-sm" @click="openModal">Update Diary</button>
 				</div>
 				<!-- <div class="flex justify-between w-full">
 					<span class="font-semibold text-black">Dr Kimberly</span> on Mount Hospital on 29 June 2021 <span class="text-violet-400 font-semibold">Prescription #2J983KTO</span>
@@ -85,5 +99,13 @@ onMounted(() => {
 					<p class="font-normal text-xs text-gray-300 w-8/12">Three sickle cell patients were sustained at normal haemoglobin levels</p>
 				</div> -->
 			</div>
+
+			<DiaryModal :isVisible="isModalVisible" @close="closeModal">
+				<form>
+					<input type="text">
+					<textarea name="" id="" cols="30" rows="10"></textarea>
+					<input type="text">
+				</form>
+			</DiaryModal>
 		</div>
 </template>
---
+++ src/components/DiaryModal.vue
@@ -0,0 +1,24 @@
+<script setup>
+const props = defineProps({
+	isVisible: {
+		type: Boolean,
+		required: true
+	}
+})
+
+const emits = defineEmits(['close'])
+
+const closeModal = () => {
+	emits('close')
+}
+</script>
+
+<template>
+  <div v-if="isVisible" class="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center">
+    <div class="bg-white p-5 rounded-lg shadow-lg">
+      <h2 class="text-xl font-semibold mb-4">How are you feeling today?</h2>
+      <slot></slot>
+      <button @click="closeModal" class="mt-4 bg-red-500 text-white px-4 py-2 rounded">Close</button>
+    </div>
+  </div>
+</template>
